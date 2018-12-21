ef720abcdfecaa8e9fec200ad46721b77b291f25
Adam Fetters <adamfetterscoder@gmail.com>
2018-12-21 12:12:24 -0500

    Added header component and changed ffavicon

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import Header from './components/Header/Header';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <header className="App-header">
-       Dayton Sikh Temple
-        </header>
+      <Header />
       </div>
     );
   }
---
+++ front/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './header.css';
+
+export default function Header() {
+  return (
+    <div>
+      <h1 className="Title">Dayton Gurdwara Sahib</h1>
+    </div>
+  )
+}
